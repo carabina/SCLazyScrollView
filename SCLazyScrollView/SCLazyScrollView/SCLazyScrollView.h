67f519e49a7538ad3027dad6105991be38f9c788
Sean Choi <krazie99@gmail.com>
2017-03-08 19:06:04 +0900

    create library project

---
+++ SCLazyScrollView/SCLazyScrollView/SCLazyScrollView.h
@@ -0,0 +1,19 @@
+//
+//  SCLazyScrollView.h
+//  SCLazyScrollView
+//
+//  Created by SeanChoi on 2017. 3. 8..
+//  Copyright © 2017년 SeanChoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCLazyScrollView.
+FOUNDATION_EXPORT double SCLazyScrollViewVersionNumber;
+
+//! Project version string for SCLazyScrollView.
+FOUNDATION_EXPORT const unsigned char SCLazyScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCLazyScrollView/PublicHeader.h>
+
+
